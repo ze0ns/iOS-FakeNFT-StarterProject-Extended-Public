2fea3829fe30df1470b537091d3d6bf2e96ab3c7
sub4ikgg <zerotanker33@gmail.com>
2026-07-28 11:27:07 +0300

    [Feature] Add Profile model, ProfileRequest and ProfileService

---
+++ iOS-FakeNFT-Extended/Services/ProfileService.swift
@@ -0,0 +1,46 @@
+//
+//  ProfileService.swift
+//  iOS-FakeNFT-Extended
+//
+//  Created by Kirill Efremov on 28.07.2026.
+//
+
+import Foundation
+
+protocol ProfileService: Sendable {
+    func loadProfile(id: String) async throws -> Profile
+    func invalidateCache() async
+}
+
+extension ProfileService {
+    func loadProfile() async throws -> Profile {
+        try await loadProfile(id: ProfileServiceImpl.currentProfileId)
+    }
+}
+
+actor ProfileServiceImpl: ProfileService {
+
+    static let currentProfileId = "1"
+
+    private let networkClient: NetworkClient
+    private var cachedProfile: Profile?
+
+    init(networkClient: NetworkClient) {
+        self.networkClient = networkClient
+    }
+
+    func loadProfile(id: String) async throws -> Profile {
+        if let cachedProfile, cachedProfile.id == id {
+            return cachedProfile
+        }
+
+        let request = ProfileRequest(id: id)
+        let profile: Profile = try await networkClient.send(request: request)
+        cachedProfile = profile
+        return profile
+    }
+
+    func invalidateCache() {
+        cachedProfile = nil
+    }
+}
