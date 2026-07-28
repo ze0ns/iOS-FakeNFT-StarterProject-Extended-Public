e216ffeecf507bad063a7ccb09cab74520d55993
sub4ikgg <zerotanker33@gmail.com>
2026-07-28 12:07:51 +0300

    [Feature] Add Profile screen

--- iOS-FakeNFT-Extended/UI/Profile/ProfileView.swift
+++ iOS-FakeNFT-Extended/UI/Profile/ProfileView.swift
@@ -9,51 +9,43 @@ import SwiftUI
 
 struct ProfileView: View {
 
-    @Environment(ServicesAssembly.self) private var servicesAssembly
-    @State private var viewModel: ProfileViewModel?
+    @State private var viewModel: ProfileViewModel
+
+    @MainActor
+    init(profileService: ProfileService) {
+        _viewModel = State(initialValue: ProfileViewModel(profileService: profileService))
+    }
 
     var body: some View {
-        content
-            .task {
-                await loadProfile()
-            }
+        NavigationStack {
+            content
+        }
+        .task {
+            await viewModel.loadProfile()
+        }
     }
 
     @ViewBuilder
     private var content: some View {
-        switch viewModel?.state ?? .loading {
+        switch viewModel.state {
         case .loading:
             ProgressView()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
         case let .loaded(profile):
             loadedContent(for: profile)
         case let .failed(message):
             Text(message)
                 .multilineTextAlignment(.center)
                 .padding(16)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
         }
     }
 
     private func loadedContent(for profile: Profile) -> some View {
-        VStack(alignment: .leading, spacing: 20) {
-            Text(profile.name)
-                .font(.headline)
-
-            Text(profile.description)
-                .font(.body)
-
-            if let websiteURL = profile.websiteURL {
-                Link(profile.website, destination: websiteURL)
-            }
-
+        VStack(alignment: .leading, spacing: 0) {
             Spacer()
         }
+        .padding(.horizontal, 16)
         .frame(maxWidth: .infinity, alignment: .leading)
-        .padding(16)
-    }
-
-    private func loadProfile() async {
-        let viewModel = viewModel ?? ProfileViewModel(profileService: servicesAssembly.profileService)
-        self.viewModel = viewModel
-        await viewModel.loadProfile()
     }
 }
