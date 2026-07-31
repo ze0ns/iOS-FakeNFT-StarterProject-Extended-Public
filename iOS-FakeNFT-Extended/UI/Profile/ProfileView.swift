d40f0871e7aa62b7b3b9cb47cf0e3e0d6a76e02c
sub4ikgg <zerotanker33@gmail.com>
2026-07-31 21:45:57 +0300

    [Fix] Align user website link to the leading edge

--- iOS-FakeNFT-Extended/UI/Profile/ProfileView.swift
+++ iOS-FakeNFT-Extended/UI/Profile/ProfileView.swift
@@ -72,6 +72,7 @@ struct ProfileView: View {
                     }
                     .font(.system(size: 15))
                     .foregroundStyle(Color.blue)
+                    .multilineTextAlignment(.leading)
                     .padding(.top, 8)
                 }
 
